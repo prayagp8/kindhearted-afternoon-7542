7bee32d75d5f1a8a2b5667c6cfa8749dad300a88
Prayag Patel <prayagp8@gmail.com>
2022-11-09 10:52:29 +0530

    project blueprint added

---
+++ ProjectTravelJunky/src/main/java/com/tj/ProjectTravelJunkyApplication.java
@@ -0,0 +1,13 @@
+package com.tj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTravelJunkyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTravelJunkyApplication.class, args);
+	}
+
+}
---
+++ ProjectTravelJunky/src/test/java/com/tj/ProjectTravelJunkyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTravelJunkyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
