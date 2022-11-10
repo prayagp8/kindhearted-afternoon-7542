e49b5d9fd54a289de6624135af51dc6906c74f4b
RANJEET KUMAR <ranjeet030200@gmail.com>
2022-11-10 09:44:57 +0530

    Some Changes

--- ProjectTravelJunky/src/main/java/com/tj/model/Customer.java
+++ ProjectTravelJunky/src/main/java/com/tj/model/Customer.java
@@ -1,10 +1,15 @@
 package com.tj.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Customer {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer customerId;
 	private String name;
 	private String costumerpassword;
--- ProjectTravelJunky/src/main/java/com/tj/model/User.java
+++ ProjectTravelJunky/src/main/java/com/tj/model/User.java
@@ -3,10 +3,12 @@ package com.tj.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class User {
 
+	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer userId;
 	private String userType;
