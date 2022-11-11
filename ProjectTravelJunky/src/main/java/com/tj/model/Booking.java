d820d8654e7f58fcedb6158d9b285184db97e23c
RANJEET KUMAR <ranjeet030200@gmail.com>
2022-11-11 11:41:34 +0530

    Logbook Add'

--- ProjectTravelJunky/src/main/java/com/tj/model/Booking.java
+++ ProjectTravelJunky/src/main/java/com/tj/model/Booking.java
@@ -7,6 +7,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
+
+
 @Entity
 public class Booking {
 	@Id
