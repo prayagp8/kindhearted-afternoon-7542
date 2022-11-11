8a1ad2948bb93ee01bb70550e73a9980a5407b0f
Gautam kumar <gautamk1336@gmail.com>
2022-11-11 14:00:41 +0530

    some fix

--- ProjectTravelJunky/src/main/java/com/tj/exception/MyErrorDetails.java
+++ ProjectTravelJunky/src/main/java/com/tj/exception/MyErrorDetails.java
@@ -3,7 +3,6 @@ package com.tj.exception;
 import java.time.LocalDateTime;
 
 public class MyErrorDetails {
-<<<<<<< HEAD
 	 private LocalDateTime timestamp;
 	 private String message;
 	 private String description;
@@ -18,12 +17,6 @@ public class MyErrorDetails {
 		this.message = message;
 		this.description = description;
 	}
-=======
-
-	private LocalDateTime timestamp;
-	private String message;
-	private String description;
->>>>>>> 869447c114c7aa7f2df8a9da9a3a57e5eaa4ea3f
 
 	public LocalDateTime getTimestamp() {
 		return timestamp;
@@ -53,21 +46,9 @@ public class MyErrorDetails {
 	public String toString() {
 		return "MyErrorDetails [timestamp=" + timestamp + ", message=" + message + ", description=" + description + "]";
 	}
-<<<<<<< HEAD
-	 
-}
-=======
 
-	public MyErrorDetails(LocalDateTime timestamp, String message, String description) {
-		super();
-		this.timestamp = timestamp;
-		this.message = message;
-		this.description = description;
-	}
+	
 
-	public MyErrorDetails() {
-		// TODO Auto-generated constructor stub
-	}
 
 }
->>>>>>> 869447c114c7aa7f2df8a9da9a3a57e5eaa4ea3f
+
